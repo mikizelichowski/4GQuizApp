61b025cc2956bdbc0529defb8ad92c0b02a56ccc
Mikolaj <mikolaj.zielichowski@neos.co.uk>
2023-10-06 12:52:20 +0200

    Create custom image helper

---
+++ 4GQuizApp/Helper/ViewStates.swift
@@ -0,0 +1,15 @@
+//
+//  ViewStates.swift
+//  4GQuizApp
+//
+//  Created by Mikolaj Zelichowski on 06/10/2023.
+//
+
+import Foundation
+
+enum ViewStates {
+    case empty
+    case loading
+    case ready
+    case error
+}
