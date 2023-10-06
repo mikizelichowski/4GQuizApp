e8e192e463b0153748557ce2dd09b1686d3b8688
Mikolaj <mikolaj.zielichowski@neos.co.uk>
2023-10-06 12:31:10 +0200

    Add rounded corner extension

---
+++ 4GQuizApp/Helper/Extensions/RoundedCorner.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedCorner.swift
+//  4GQuizApp
+//
+//  Created by Mikolaj Zelichowski on 06/10/2023.
+//
+
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+/// Custom RoundedCorner shape used for cornerRadius extension above
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
