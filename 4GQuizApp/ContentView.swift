67a3ca3743d2610d0bf58d8a37567d5c1065fe5a
Mikolaj <mikolaj.zielichowski@neos.co.uk>
2023-10-06 09:53:06 +0200

    Add icon and colors

--- 4GQuizApp/ContentView.swift
+++ 4GQuizApp/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("4G")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
