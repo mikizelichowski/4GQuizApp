cbf0c6fb04f27cfc230e11a5844c49b885901ba7
Mikolaj <mikolaj.zielichowski@neos.co.uk>
2023-10-06 09:59:55 +0200

    Create api constants

---
+++ 4GQuizApp/Network/APIConstants/APIConstants.swift
@@ -0,0 +1,13 @@
+//
+//  APIConstants.swift
+//  4GQuizApp
+//
+//  Created by Mikolaj Zelichowski on 06/10/2023.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseURL = "https://quiz.o2.pl/api/v1/quizzes/0/100"
+    static let detailQuizURL = "https://quiz.o2.pl/api/v1/quiz/"
+}
---
+++ 4GQuizApp/Network/NetworkManager/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  4GQuizApp
+//
+//  Created by Mikolaj Zelichowski on 06/10/2023.
+//
+
+import Foundation
