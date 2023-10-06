427def9f7dccdee92dbf8b58a0cb7e70b6a4dd40
Mikolaj <mikolaj.zielichowski@neos.co.uk>
2023-10-06 09:09:03 +0200

    Initial Commit

---
+++ 4GQuizApp/_GQuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _GQuizAppApp.swift
+//  4GQuizApp
+//
+//  Created by Mikolaj Zelichowski on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct _GQuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
